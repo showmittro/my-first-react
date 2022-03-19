9cba91a1b0b813704156e3fca8fc3fb9ede95df1
showmittro <showmittrobiswas2000@gmail.com>
2022-03-19 11:36:35 +0600

    full code added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks =["Rubel", "BappaRaz", "Kuber", "Jashim", "Salman Shah","Razzak","Riyaz"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayka="Mousomi"></Person>
+      <Person name={nayoks[1]} nayka = "cheka"></Person>
+      <Person name="Kuber" nayka ="kopila"></Person>
+      <Friend name= "Rumon" nayka="oxyzen"></Friend> */}
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
 }
+
+function Person (props){
+  return (
+      <div className='person'>
+        <h1>{props.name}</h1>
+        <p>{props.nayka}</p>
+      </div>
+  )
+  
+}
+
+function Friend (props){
+  return(
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>Nayka:{props.nayka}</p>
+    </div>
+  )
+}
+
+
 
 export default App;
